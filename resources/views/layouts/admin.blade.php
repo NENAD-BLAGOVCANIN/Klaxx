8b422f2900ebce4d8639b3a81b7b11d8fe50063e
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-31 05:08:20 +0200

    updates

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Smarty v5</title>
+    <meta name="description" content="...">
+
+    <meta name="viewport" content="width=device-width, maximum-scale=5, initial-scale=1">
+
+    <link rel="dns-prefetch" href="https://fonts.googleapis.com/">
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com/">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link rel="preload" href="assets/fonts/flaticon/Flaticon.woff2" as="font" type="font/woff2" crossorigin>
+
+    <link rel="stylesheet" href="/css/core.min.css">
+
+    <link rel="stylesheet" href="/css/vendor_bundle.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="demo.files/logo/icon_512x512.png">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        @yield('content')
+
+        <script src="assets/js/core.min.js"></script>
+
+    </div>
+
+</body>
+
+</html>
