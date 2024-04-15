2010d3e6b6e7f1464dcc505ec525deaf21888b76
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-04-15 02:53:34 +0200

    updates

---
+++ database/migrations/2024_04_15_004606_create_category_attributes_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('category_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('category_attributes');
+    }
+};
