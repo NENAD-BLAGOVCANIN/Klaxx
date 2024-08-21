b9e55a02c4280994ee09d79b79f50a52d20f19a3
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-08-21 13:32:28 +0200

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <link rel="preload" href="/fonts/flaticon/Flaticon.woff2" as="font" type="font/woff2" crossorigin>
 
-    <link rel="stylesheet" href="/css/core.min.css?v=5">
+    <link rel="stylesheet" href="/css/core.min.css?v=6">
 
     <link rel="stylesheet" href="/css/vendor_bundle.min.css?v=4">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet">
